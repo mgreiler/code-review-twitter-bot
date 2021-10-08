d3f35e8deab2a4c3d669ad682baffe9dfdab81c3
Michaela Greiler-Göritzer <michaela.greiler@gmail.com>
2021-10-08 10:55:55 +0200

    Extract phrases into array

--- code-review-bot/matching_rules.py
+++ code-review-bot/matching_rules.py
@@ -1,4 +1,3 @@
-
 gaming_related_phrases = [
     "game"
     "nintendo"
@@ -36,7 +35,7 @@ def contains_gaming_related_phrases(tweet_text):
     for phrase in gaming_related_phrases:
         if phrase in tweet_text:
             return True
-        return False
+    return False
 
 
 def contains_tweets_that_come_up_too_often(tweet_text):
@@ -57,16 +56,22 @@ def contains_unrelated_phrases(tweet_text):
     return False
 
 
+conspiracy_phrases = [
+    "https://lockdownsceptics.g/code-review-of-fergusons-model/",
+    "ferguson",
+    "lockdown sceptics",
+    "imperial college model",
+    "covid",
+    "cona",
+    "lockdownsceptics",
+    "crypto"
+]
+
+
 def contains_conspiracy_phrases(tweet_text):
-    if ("https://lockdownsceptics.org/code-review-of-fergusons-model/" in tweet_text) \
-            or "ferguson" in tweet_text \
-            or "lockdown sceptics" in tweet_text \
-            or "imperial college model" in tweet_text \
-            or "covid" in tweet_text \
-            or "corona" in tweet_text \
-            or "lockdownsceptics" in tweet_text \
-            or "crypto" in tweet_text:
-        return True
+    for phrase in conspiracy_phrases:
+        if phrase in tweet_text:
+            return True
     return False
 
 
@@ -76,14 +81,20 @@ def contains_inappropriate_phrases(tweet_text):
     return False
 
 
+marketing_related_phrases = [
+    "product review"
+    "discount",
+    "referral code",
+    "promo code",
+    "medici code review",
+    "kibo code review"
+]
+
+
 def contains_marketing_related_phrases(tweet_text):
-    if "product review" in tweet_text \
-            or "discount" in tweet_text \
-            or "referral code" in tweet_text \
-            or "promo code" in tweet_text \
-            or "medici code review" in tweet_text \
-            or "kibo code review" in tweet_text:
-        return True
+    for phrase in marketing_related_phrases:
+        if phrase in tweet_text:
+            return True
     return False
 
 
@@ -94,20 +105,24 @@ def contains_ai_related_phrases(tweet_text):
     return False
 
 
+code_review_phrases = [
+    "code review",
+    "reviewing code",
+    "codereview",
+    "review code",
+    "review the code",
+    "review your code",
+    "review someone's code",
+    "review his code",
+    " pr freview",
+    " pr review ",
+    "pull request",
+    "merge request"
+]
+
+
 def contains_code_review_phrase(tweet_text):
-    # logger.info("Looking for exact match")
-    if "code review" in tweet_text \
-            or "reviewing code" in tweet_text \
-            or "codereview" in tweet_text \
-            or "review code" in tweet_text \
-            or "review the code" in tweet_text \
-            or "review your code" in tweet_text \
-            or "review someone's code" in tweet_text \
-            or "review his code" in tweet_text \
-            or " pr for review" in tweet_text \
-            or (" pr review " in tweet_text) \
-            or ("pull request" in tweet_text and "review" in tweet_text) \
-            or ("merge request" in tweet_text and "review" in tweet_text):
-        # logger.info("Exact Match found in: " + tweet_text)
-        return True
+    for phrase in code_review_phrases:
+        if phrase in tweet_text:
+            return True
     return False
